1766d498c71fb22bd11a77d491f55ecdc063d191
Aguss-Coder <luduenagustin615@gmail.com>
2022-07-12 02:11:02 -0300

    home and about section ended

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
     './main.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'robo': ['Roboto', 'sans-serif']
+      },
+      backgroundImage: {
+        'code-pattern': 'url(https://1.bp.blogspot.com/-MGS7Y82aSog/YB5oegC37_I/AAAAAAAAADI/wTiHDISzDYokAClfkIJIaVMYgVg3Xqc3ACLcBGAsYHQ/s1280/n.jpg)'
+      }
+    },
+    colors: {
+      'principal-50': '#e7ecfe',
+      'principal-100': '#b6c6fc',
+      'principal-200': '#85a0fa;',
+      'principal-300': '#547af8',
+      'principal': '#2354f6',
+      'principal-400': '#093adc',
+      'principal-500': '#072dab',
+      'principal-600': '#05207a',
+      'principal-700': '#031349',
+      'principal-800': '#010618',
+      'black': 'rgba(0, 0, 0, .7)',
+      'gray': '#414141;',
+      'ht': '#f1662a',
+      'cs': '#1c88c7',
+      'js': '#f4de18',
+      'py': '#356f9e'
+    }
   },
   plugins: [],
 }
